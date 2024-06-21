0e15a372fb9642327275bde55213ff4f4fda19a1
Raghunadh Barlapudi <81347796+9394113857@users.noreply.github.com>
2024-06-21 18:58:38 +0530

    The last error we were addressing was related to the ngx-slickjs module and its TypeScript typings issue. So on this still working not yet done so far.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,28 @@
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'; // Import FormsModule and ReactiveFormsModule
+
 import { AuthService } from './services/auth.service';
 import { FirstPharmacyService } from './services/first-pharmacy.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    
-    
+    // Add other components here
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    FormsModule,
-    ReactiveFormsModule,
     HttpClientModule,
     RouterModule,
-    
-    
-    
+    FormsModule, // Import FormsModule if you need template-driven forms
+    ReactiveFormsModule, // Import ReactiveFormsModule for reactive forms
   ],
   providers: [
     AuthService,
